1b0fc0a2c270ed3323424af618a22c2e0c2b101b
Pedro Antonio Ruiz Cuesta <pruiz@Pedros-iMac.local>
2019-03-30 12:15:02 -0700

    Se crea la vista y se enlaza con VC

--- XYZ-App/XYZ-App/controller/ViewController.swift
+++ XYZ-App/XYZ-App/controller/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var xLabel: UILabel!
+    @IBOutlet weak var yLabel: UILabel!
+    @IBOutlet weak var zLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
